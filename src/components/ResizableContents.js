8d24bec6500508b965c8e2e8482a2913605cfc86
pixysq <bishtkrishnam400@gmail.com>
2024-05-12 18:00:37 +0530

    changes

--- src/components/ResizableContents.js
+++ src/components/ResizableContents.js
@@ -6,156 +6,177 @@ import UserData from './UserData';
 import SampleImg from '../sample.jpg';
 
 const ResizableContents = () => {
-	const [showModal, setShowModal] = useState(false); // State to manage modal visibility
-	const [updateData, setUpdateData] = useState(null); // State to store data for update
+    const [showModal, setShowModal] = useState(false); // State to manage modal visibility
+    const [updateData, setUpdateData] = useState(null); // State to store data for update
 
-	useEffect(() => {
-		// Fetch user data from the backend when the component mounts
-		fetchUserData();
-	}, [showModal]);
+    useEffect(() => {
+        // Fetch user data from the backend when the component mounts
+        fetchUserData();
+    }, [showModal]);
 
-	// Function to fetch user data from the backend
-	const fetchUserData = async () => {
-		try {
-			const response = await fetch('https://layout-app-backend.onrender.com/api/data1');
-			if (!response.ok) {
-				throw new Error('Failed to fetch user data');
-			}
-			const userData = await response.json();
+    // Function to fetch user data from the backend
+    const fetchUserData = async () => {
+        try {
+            const response = await fetch('https://layout-app-backend.onrender.com/api/data1');
+            if (!response.ok) {
+                throw new Error('Failed to fetch user data');
+            }
+            const userData = await response.json();
 
-			// Setting the user data to the last element
-			if (Array.isArray(userData) && userData.length > 0) {
-				setUpdateData(userData[userData.length - 1]);
-			}
-		} catch (error) {
-			console.error('Error fetching user data:', error);
-		}
-	};
+            // Setting the user data to the last element
+            if (Array.isArray(userData) && userData.length > 0) {
+                setUpdateData(userData[userData.length - 1]);
+            }
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    };
 
-	const handleModal = (isUpdate) => {
-		if (isUpdate) {
-			setShowModal(true);
-		} else {
-			setUpdateData(null);
-			setShowModal(true);
-		}
-	};
+    const handleModal = (isUpdate) => {
+        if (isUpdate) {
+            setShowModal(true);
+        } else {
+            setUpdateData(null);
+            setShowModal(true);
+        }
+    };
 
-	const {
-		isDragging: isTerminalDragging,
-		position: terminalH,
-		splitterProps: terminalDragBarProps
-	} = useResizable({
-		axis: 'y',
-		initial: 200,
-		min: 50,
-		reverse: true
-	});
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
 
-	const {
-		isDragging: isPluginDragging,
-		position: pluginW,
-		splitterProps: pluginDragBarProps
-	} = useResizable({
-		axis: 'x',
-		initial: 500,
-		min: 50,
-		reverse: true
-	});
+        const formData = new FormData(event.target);
 
-	const handleFormSubmit = async (event) => {
-		event.preventDefault();
+        try {
+            const url = updateData ? `https://layout-app-backend.onrender.com/api/data1/${updateData._id}` : 'https://layout-app-backend.onrender.com/api/data1/add';
+            const method = updateData ? 'PUT' : 'POST';
 
-		const formData = new FormData(event.target);
+            const response = await fetch(url, {
+                method: method,
+                body: formData
+            });
 
-		try {
-			const url = updateData ? `https://layout-app-backend.onrender.com/api/data1/${updateData._id}` : 'https://layout-app-backend.onrender.com/api/data1/add';
-			const method = updateData ? 'PUT' : 'POST';
+            if (!response.ok) {
+                throw new Error('Failed to save changes');
+            }
+            setShowModal(false);
+        } catch (error) {
+            console.error('Error saving changes:', error);
+        }
+    };
 
-			const response = await fetch(url, {
-				method: method,
-				body: formData
-			});
+    // Function to handle reset
+    const handleReset = async () => {
+        try {
+            const response = await fetch('https://layout-app-backend.onrender.com/api/data1/reset', {
+                method: 'DELETE'
+            });
 
-			if (!response.ok) {
-				throw new Error('Failed to save changes');
-			}
-			setShowModal(false);
+            if (!response.ok) {
+                throw new Error('Failed to reset data');
+            }
 
-		} catch (error) {
-			console.error('Error saving changes:', error);
-		}
-	};
+            // Reset user data
+            setUpdateData(null);
+        } catch (error) {
+            console.error('Error resetting data:', error);
+        }
+    };
 
-	return (
-		<>
-			<div className={"flex flex-column h-screen bg-dark font-mono color-white overflow-hidden"}>
-				<div className={"flex grow"}>
-					<div className={"flex grow"}>
-						<div className={"grow bg-darker contents box1"}>
-							{updateData ? (
-								<UserData
-									userData={updateData}
-									handleModal={handleModal}
-								/>
-							) : (
+    const {
+        isDragging: isTerminalDragging,
+        position: terminalH,
+        splitterProps: terminalDragBarProps
+    } = useResizable({
+        axis: 'y',
+        initial: 200,
+        min: 50,
+        reverse: true
+    });
+
+    const {
+        isDragging: isPluginDragging,
+        position: pluginW,
+        splitterProps: pluginDragBarProps
+    } = useResizable({
+        axis: 'x',
+        initial: 500,
+        min: 50,
+        reverse: true
+    });
+
+    return (
+        <>
+            <div className={"flex flex-column h-screen bg-dark font-mono color-white overflow-hidden"}>
+                <div className={"flex grow"}>
+                    <div className={"flex grow"}>
+                        <div className={"grow bg-darker contents box1"}>
+                            {updateData ? (
 								<>
-									<img src={SampleImg} alt="User" />
-									<h3>Hello, My name is John Doe and I'm 18 years old living in New York City</h3>
-									<div className='btns'>
-										<button className='add-btn' onClick={() => handleModal()}>Add</button>
-									</div>
+                                <UserData
+                                    userData={updateData}
+                                    handleModal={handleModal}
+                                />
+								{/* Reset button */}
+								<button className="btn btn-danger reset-btn" onClick={handleReset}>Reset Data</button>
 								</>
-							)}
-						</div>
-						<Resizer isDragging={isPluginDragging} {...pluginDragBarProps} />
-						<div className={cn("contents box2", isPluginDragging && "dragging")} style={{ width: pluginW }}>
-							More Data
-						</div>
-					</div>
-				</div>
-				<Resizer dir={"horizontal"} isDragging={isTerminalDragging} {...terminalDragBarProps} />
-				<div className={cn("shrink-0 bg-darker contents", isTerminalDragging && "dragging")} style={{ height: terminalH }}>
-					More Data
-				</div>
-			</div>
+                            ) : (
+                                <>
+                                    <img src={SampleImg} alt="User" />
+                                    <h3>Hello, My name is John Doe and I'm 18 years old living in New York City</h3>
+                                    <div className='btns'>
+                                        <button className='add-btn' onClick={() => handleModal()}>Add</button>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                        <Resizer isDragging={isPluginDragging} {...pluginDragBarProps} />
+                        <div className={cn("contents box2", isPluginDragging && "dragging")} style={{ width: pluginW }}>
+                            More Data
+                        </div>
+                    </div>
+                </div>
+                <Resizer dir={"horizontal"} isDragging={isTerminalDragging} {...terminalDragBarProps} />
+                <div className={cn("shrink-0 bg-darker contents", isTerminalDragging && "dragging")} style={{ height: terminalH }}>
+                    More Data
+                </div>
+            </div>
 
-			{/* Modal */}
-			<div className={`modal ${showModal ? 'show' : ''}`} tabIndex="-1" style={{ display: showModal ? 'block' : 'none' }}>
-				<div className="modal-dialog">
-					<div className="modal-content">
-						<div className="modal-header">
-							<h5 className="modal-title" id="exampleModalLabel">{updateData ? 'Update' : 'Add New'} Content</h5>
-							<button type="button" className="btn-close" onClick={() => setShowModal(false)}></button>
-						</div>
-						<div className="modal-body">
-							<form onSubmit={handleFormSubmit} encType="multipart/form-data">
-								<div className="mb-3">
-									<label htmlFor="name" className="form-label">Name</label>
-									<input type="text" className="form-control" id="name" name='name' defaultValue={updateData?.name} />
-								</div>
-								<div className="mb-3">
-									<label htmlFor="age" className="form-label">Age</label>
-									<input type="text" className="form-control" id="age" name='age' defaultValue={updateData?.age} />
-								</div>
-								<div className="mb-3">
-									<label htmlFor="place" className="form-label">Place</label>
-									<input type="text" className="form-control" id="place" name='place' defaultValue={updateData?.place} />
-								</div>
-								<div className="mb-3">
-									<label htmlFor="imageUrl" className="form-label">Image Url</label>
-									<input type="file" accept="images/*" className="form-control" id="imageUrl" name='imageUrl' />
-								</div>
-								<button type="submit" className="btn btn-primary">Save changes</button>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-			{/* Modal backdrop */}
-			<div className={`modal-backdrop ${showModal ? 'show' : ''}`} style={{ display: showModal ? 'block' : 'none' }}></div>
-		</>
-	);
+            {/* Modal */}
+            <div className={`modal ${showModal ? 'show' : ''}`} tabIndex="-1" style={{ display: showModal ? 'block' : 'none' }}>
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">{updateData ? 'Update' : 'Add New'} Content</h5>
+                            <button type="button" className="btn-close" onClick={() => setShowModal(false)}></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={handleFormSubmit} encType="multipart/form-data">
+                                <div className="mb-3">
+                                    <label htmlFor="name" className="form-label">Name</label>
+                                    <input type="text" className="form-control" id="name" name='name' defaultValue={updateData?.name} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="age" className="form-label">Age</label>
+                                    <input type="text" className="form-control" id="age" name='age' defaultValue={updateData?.age} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="place" className="form-label">Place</label>
+                                    <input type="text" className="form-control" id="place" name='place' defaultValue={updateData?.place} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="imageUrl" className="form-label">Image Url</label>
+                                    <input type="file" accept="images/*" className="form-control" id="imageUrl" name='imageUrl' />
+                                </div>
+                                <button type="submit" className="btn btn-primary">Save changes</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Modal backdrop */}
+            <div className={`modal-backdrop ${showModal ? 'show' : ''}`} style={{ display: showModal ? 'block' : 'none' }}></div>
+        </>
+    );
 };
 
 export default ResizableContents;
