8b720857a82b805d283f527e41d80469a36a301f
pixysq <bishtkrishnam400@gmail.com>
2024-05-12 17:29:25 +0530

    changes

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -6,7 +6,7 @@ const UserData = ({ userData, handleModal }) => {
 		<>
 			{userData && (
 				<>
-					<img src={`http://localhost:8080/uploads/${userData.imageUrl}`} alt="User" />
+					<img src={`https://layout-app-backend.onrender.com/uploads/${userData.imageUrl}`} alt="User" />
 					<h3 className='w-50'>Hello, My name is {userData.name} and I'm {userData.age} years old living in {userData.place}</h3>
 					<div className='btns'>
 						{/* Use handleModal directly */}
